271dd6b787ee1e7c1194c2cbf93e797fbd7e079b
Kvazar7 <Kometa737@gmail.com>
2023-05-03 11:17:20 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { getContacts, addContact, deleteContact } from 'services/services';
 
 const handlePending = state => {
